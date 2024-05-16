42f78f88ab0b8cb4d35d02527c53ae29a6d1141f
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-05-17 00:23:33 +0600

    create initial setup and created routs,layout,components,pages

---
+++ src/Components/Category.jsx
@@ -0,0 +1,59 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import slide1 from "../assets/home/slide1.jpg";
+import slide2 from "../assets/home/slide2.jpg";
+import slide3 from "../assets/home/slide3.jpg";
+import slide4 from "../assets/home/slide4.jpg";
+import slide5 from "../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3 className="text-2xl text-center uppercase text-white shadow-sm -mt-10">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3 className="text-2xl text-center uppercase text-white shadow-sm -mt-10">
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-2xl text-center uppercase text-white shadow-sm -mt-10">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3 className="text-2xl text-center uppercase text-white shadow-sm -mt-10">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h3 className="text-2xl text-center uppercase text-white drop-shadow-xl -mt-10">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../../Components/Banner";
+import Category from "../../Components/Category";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Category/>
+        </div>
+    );
+};
+
+export default Home;
